12692a77d6a5ba5f107a2bba04e51322c22888b8
Oussama Aouina <oussamaaouina99@gmail.com>
2024-05-08 16:25:58 +0100

    all works

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TicketRepository::class)]
+class Ticket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $prix = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $emplacement = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $choisi = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tickets')]
+    private ?Categorie $categorie = null;
+
+
+
+    public function __construct()
+    {
+        $this->categ = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(?float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getEmplacement(): ?string
+    {
+        return $this->emplacement;
+    }
+
+    public function setEmplacement(?string $emplacement): static
+    {
+        $this->emplacement = $emplacement;
+
+        return $this;
+    }
+
+    public function isChoisi(): ?bool
+    {
+        return $this->choisi;
+    }
+
+    public function setChoisi(?bool $choisi): static
+    {
+        $this->choisi = $choisi;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): static
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+
+}
